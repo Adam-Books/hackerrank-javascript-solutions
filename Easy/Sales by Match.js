7acb79b5e3a7a8c86669a30cf09374dec2e179ae
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2021-12-07 20:24:13 +0200

    Update Sales by Match.js

--- Easy/Sales by Match.js
+++ Easy/Sales by Match.js
@@ -6,9 +6,10 @@ Description:
   There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
 
   Example
-
-
-  There is one pair of color  and one of color . There are three odd socks left, one of each color. The number of pairs is .
+  n = 7
+  ar = [1, 2, 1, 2, 1, 3, 2]
+  There is one pair of color 1 and one of color 2. 
+  There are three odd socks left, one of each color. The number of pairs is 2.
 
   Function Description
 
@@ -81,14 +82,12 @@ Description:
   Wrong Answer
   Input (stdin)
 
-  Download
   9
   10 20 20 10 10 30 50 10 20
   Your Output (stdout)
   NaN
   Expected Output
 
-  Download
   3
   Contest CalendarBlogScoring
 
@@ -98,4 +97,19 @@ Link:
 */
 
 // Solutions:
+
 // Solution1:
+const sockMerchant = (n, ar) => {
+    let pairs = 0;
+    const sortedAr = ar.sort((a, b) => a - b);
+    for (let i = 0; i < n; i++) {
+        if (sortedAr[i] == sortedAr[i + 1]) {
+            pairs++;
+            i++;
+        }
+    }
+    return pairs;
+}
+
+
+
