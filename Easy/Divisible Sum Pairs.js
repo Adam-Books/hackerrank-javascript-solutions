80dd348dba640b0653cc7b487c487d80a354d7d5
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2021-12-20 19:10:36 +0200

    Divisible Sum Pairs - JavaScript Solution

--- Easy/Divisible Sum Pairs.js
+++ Easy/Divisible Sum Pairs.js
@@ -50,3 +50,15 @@ Link:
 // Solutions:
 
 // Solution 1:
+function divisibleSumPairs(n, k, ar) {
+  let counter = 0;
+  for (let i = 0; i < n; i++) {
+    ar.slice(i + 1).filter((item) => {
+      if ((item + ar[i]) % k == 0) {
+        counter++;
+      }
+    });
+  }
+  return counter;
+}
+
