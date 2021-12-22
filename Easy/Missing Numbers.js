bb902231376086ce1d40c48953f96c8aaf244d9c
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2021-12-22 17:29:05 +0200

    Missing Numbers - JavaScript Solution

--- Easy/Missing Numbers.js
+++ Easy/Missing Numbers.js
@@ -65,4 +65,31 @@ Link:
 // Solutions:
 // Solution 1:
 
+const missingNumbers = (array1, array2) => {
+  let numHash1 = new Map();
+  let numHash2 = new Map();
+
+  for (let i = 0; i < array1.length; i++) {
+    let count = numHash1.get(array1[i]);
+    if (!count) numHash1.set(array1[i], 1);
+    else numHash1.set(array1[i], count + 1);
+  }
+
+  for (let i = 0; i < array2.length; i++) {
+    let count = numHash2.get(array2[i]);
+    if (!count) numHash2.set(array2[i], 1);
+    else numHash2.set(array2[i], count + 1);
+  }
+
+  let missingNumbers = [];
+  for (let key of numHash2.keys()) {
+    let countB = numHash2.get(key);
+    if (!numHash1.get(key) || countB > numHash1.get(key))
+      missingNumbers.push(key);
+  }
+
+  return missingNumbers.sort((a, b) => a - b);
+};
+
+
 
