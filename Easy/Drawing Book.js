73034922f8c346991fba2360b9ecc97fc2160bf5
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2021-12-29 11:10:52 +0200

    Update Drawing Book.js

--- Easy/Drawing Book.js
+++ Easy/Drawing Book.js
@@ -3,11 +3,17 @@ Title:
   Drawing Book
   
 Description:
-  A teacher asks the class to open their books to a page number. A student can either start turning pages from the front of the book or from the back of the book. They always turn pages one at a time. When they open the book, page  is always on the right side:
+  A teacher asks the class to open their books to a page number. 
+  A student can either start turning pages from the front of the book or from the back of the book. 
+  They always turn pages one at a time. When they open the book, page  is always on the right side:
 
   image
 
-  When they flip page , they see pages  and . Each page except the last page will always be printed on both sides. The last page may only be printed on the front, given the length of the book. If the book is  pages long, and a student wants to turn to page , what is the minimum number of pages to turn? They can start at the beginning or the end of the book.
+  When they flip page , they see pages  and . 
+  Each page except the last page will always be printed on both sides. 
+  The last page may only be printed on the front, given the length of the book. 
+  If the book is  pages long, and a student wants to turn to page , 
+  what is the minimum number of pages to turn? They can start at the beginning or the end of the book.
 
   Given  and , find and print the minimum number of pages that must be turned in order to arrive at page .
 
@@ -17,7 +23,9 @@ Description:
 
   Untitled Diagram(4).png
 
-  Using the diagram above, if the student wants to get to page , they open the book to page , flip  page and they are on the correct page. If they open the book to the last page, page , they turn  page and are at the correct page. Return .
+  Using the diagram above, if the student wants to get to page , they open the book to page , 
+  flip  page and they are on the correct page. 
+  If they open the book to the last page, page , they turn  page and are at the correct page. Return .
 
   Function Description
 
